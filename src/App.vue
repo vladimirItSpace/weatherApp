e9844bbf3d5e8e6dc6e67c727499f302e7089bca
vladimir <vova.miron.2@gmail.com>
2023-08-04 15:06:39 +0300

    Correct error

--- src/App.vue
+++ src/App.vue
@@ -48,71 +48,8 @@ export default {
     <div class="result_app">
       <p>Weather in {{ cityName}} for today</p>
 
-      <p v-show="info != null">{{info.main.temp}} Grade</p>
+      <p v-show="this.info != null">{{info.main.temp}} Grade</p>
     </div>
   </div>
 </template>
 
-
-
-<style scoped>
-.wrapper{
-  width: 300px;
-  height: 400px;
-  border-radius: 50px;
-  background: #66d6e1;
-
-  text-align: center;
-  
-}
-
-.header_app{
-  text-align: center;
-  padding-top: 10px;
-}
-
-.input_app{
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  gap: 15px;
-  
-  margin-top: 35px;
-}
-
-.input_app input{
-  background: transparent;
-  padding: 5px 8px;
-  border: 0;
-  border-bottom: 1px black solid;
-
-  color: white;
-  font-size: 14px;
-  outline: none;
-}
-
-.input_app input:focus{
-  
-  border-bottom: 2px rgb(161, 0, 0) solid;
-
-}
-
-.input_app button{
-  padding: 10px 13px;
-  border: 0;
-  background-color: #ffffff;
-  border-radius: 50px;
-  cursor: pointer;
-
-}
-
-.result_app{
-  text-align: center;
-  margin-top: 35px;
-}
-
-.error_app{
-  margin-top: 5px;
-  color: red;
-}
-</style>
